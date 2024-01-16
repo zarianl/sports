a17b8c09985a019e794ddf14b7bf0f391b161f86
Zarian Lewis <zarian.a.lewis@gmail.com>
2024-01-16 14:39:01 -0600

    fix possibly null

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -142,8 +142,8 @@ const GamesPage: React.FC<GamesPageProps> = () => {
                     <TableCell>{getPredictedScore(game)}</TableCell>
 
                     <TableCell>{game.estimatedHalfLine ?? " "}</TableCell>
-                    <TableCell>{game.awayPeriods[0] != null ? game.awayPeriods[0] : " "}</TableCell>
-                    <TableCell>{game.homePeriods[0] ?? " "}</TableCell>
+                    <TableCell>{game.awayPeriods?.[0] ?? " "}</TableCell>
+                    <TableCell>{game.homePeriods?.[0] ?? " "}</TableCell>
                     <TableCell>{game.actualHalfScore ?? " "}</TableCell>
                     <TableCell>{game.winLoss}</TableCell>
                   </TableRow>
